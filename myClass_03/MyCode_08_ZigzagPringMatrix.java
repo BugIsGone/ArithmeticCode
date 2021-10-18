6883538cae75bd12375e10092734eb42724027c3
LiBai <shapemind@163.com>
2021-10-19 00:05:32 +0800

    MPC_20211019_1

--- myClass_03/MyCode_08_ZigzagPringMatrix.java
+++ myClass_03/MyCode_08_ZigzagPringMatrix.java
@@ -6,19 +6,38 @@ package myClass_03;
  */
 public class MyCode_08_ZigzagPringMatrix {
     public static void main(String[] args) {
-
+        int[][] matrix = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+        printMatrixZigzag(matrix);
     }
 
     public static void printMatrixZigzag(int[][] matrix) {
-        int topR = 0;
-        int topC = 0;
-        int downR = 0;
-        int downC = 0;
+        int row1 = 0;
+        int col1 = 0;
+        int row2 = 0;
+        int col2 = 0;
         int endR = matrix.length - 1;
         int endC = matrix[0].length - 1;
         boolean fromUp = false;
-        while () {
-            
+        while (row1 <= endR) {
+            printLevel(matrix, row1, col1, row2, col2, fromUp);
+            row1 = col1 == endC ? ++row1 : row1;
+            col1 = col1 == endC ? col1 : ++col1;
+            col2 = row2 == endR ? ++col2 : col2;
+            row2 = row2 == endR ? row2 : ++row2;
+            fromUp = !fromUp;
+        }
+        System.out.println();
+    }
+
+    private static void printLevel(int[][] matrix, int topR, int topC, int downR, int downC, boolean fromUp) {
+        if (fromUp) {
+            while (topR <= downR) {
+                System.out.print(matrix[topR++][topC--] + " ");
+            }
+        } else {
+            while (downR >= topR) {
+                System.out.print(matrix[downR--][downC++] + " ");
+            }
         }
     }
 }
