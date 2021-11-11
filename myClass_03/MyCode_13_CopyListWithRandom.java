78a66af521efa06e269819f415f1a3067d2416b8
shapemind@aliyun.com <shapemind@163.com>
2021-11-11 19:35:20 +0800

    20211109-CPC-1

--- myClass_03/MyCode_13_CopyListWithRandom.java
+++ myClass_03/MyCode_13_CopyListWithRandom.java
@@ -1,7 +1,6 @@
 package myClass_03;
 
-import org.omg.CORBA.NO_IMPLEMENT;
-
+import javax.sound.midi.SoundbankResource;
 import java.util.HashMap;
 
 /**
@@ -18,7 +17,36 @@ import java.util.HashMap;
  */
 public class MyCode_13_CopyListWithRandom {
     public static void main(String[] args) {
+        Node head = null;
+        Node res1 = null;
+        Node res2 = null;
+        printRandLinkedList(head);
+        res1 = copyListWithRand1(head);
+        printRandLinkedList(res1);
+        res2 = copyListWithRand2(head);
+        printRandLinkedList(res2);
+        System.out.println("========================");
 
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
+
+        head.rand = head.next.next.next.next.next; // 1 -> 6
+        head.next.rand = head.next.next.next.next.next; // 2 -> 6
+        head.next.next.rand = head.next.next.next.next; // 3 -> 5
+        head.next.next.next.rand = head.next.next; // 4 -> 3
+        head.next.next.next.next.rand = null; // 5 -> null
+        head.next.next.next.next.next.rand = head.next.next.next;
+
+        printRandLinkedList(head);
+        res1 = copyListWithRand1(head);
+        printRandLinkedList(res1);
+        res2 = copyListWithRand2(head);
+        printRandLinkedList(head);
+        printRandLinkedList(res2);
     }
 
     public static class Node {
@@ -31,6 +59,7 @@ public class MyCode_13_CopyListWithRandom {
         }
     }
 
+    //【题目1】
     public static Node copyListWithRand1 (Node head) {
         if (head == null) return null;
         HashMap<Node, Node> map = new HashMap<>();
@@ -49,9 +78,63 @@ public class MyCode_13_CopyListWithRandom {
         return map.get(head);
     }
 
+    //【题目2】：【1】-【2】-【3】-【4】变成【1】-【1'】-【2】-【2'】-【3】-【3'】-【4】-【4'】,然后变更指针指向完成
+    //rand、next指针的复制
     public static Node copyListWithRand2 (Node head) {
         if (head == null) return null;
+        Node cur = head;
+        Node next = null;
+        //【1'】、【2'】、【3'】、【4】连接到原来的链表中
+        while (cur != null) {
+            next = cur.next;
+            cur.next = new Node(cur.value);
+            cur.next.next = next;
+            cur = next;
+        }
 
-        return null;
+        //拷贝rand
+        cur = head;
+        while (cur != null) {
+            //当cur.rand为null时，cur.rand.next就会报错，此写法不对。cur.next.rand = cur.rand.next;
+            cur.next.rand = cur.rand == null ? null : cur.rand.next;
+            cur = cur.next.next;
+        }
+
+        //链表和链表copy分离，实际是把next指针纠正
+        cur = head;
+        Node res = cur.next;
+        //搞不懂这里为什么错
+        while (cur != null) {
+            cur.next = cur.next.next;
+            res.next = cur.next == null ? null : cur.next.next;
+            cur = cur.next.next;
+        }
+        Node curCopy = null;
+        while (cur != null) {
+            next = cur.next.next;
+            curCopy = cur.next;
+            cur.next = next;
+            curCopy.next = next != null ? next.next : null;
+            cur = next;
+        }
+        return res;
+    }
+
+    //打印链表看看
+    public static void printRandLinkedList(Node head) {
+        Node cur = head;
+        System.out.print("order: ");
+        while (cur != null) {
+            System.out.print(cur.value + " ");
+            cur = cur.next;
+        }
+        cur = head;
+        System.out.println();
+        System.out.print("rand:  ");
+        while (cur != null) {
+            System.out.print((cur.rand == null ? "-" : cur.rand.value) + " ");
+            cur = cur.next;
+        }
+        System.out.println();
     }
 }
---
+++ myClass_03/MyCode_14_FindFirstIntersectNode.java
@@ -0,0 +1,30 @@
+package myClass_03;
+
+
+import basic_class_03.Code_13_CopyListWithRandom;
+
+/**
+ * @author shapemind
+ * @create 2021-11-11 17:13
+ *
+ * 两个单链表相交的一系列问题
+ * 【题目】
+ * 在本题中，单链表可能有环，也可能无环。给定两个单链表的头节点head1和head2，这两个链表可能相交，也可能不相交。
+ * 请实现一个函数，如果两个链表相交，请返回相交的第一个节点；如果不相交，返回null即可。
+ * 要求：如果链表1的长度为N，链表2的长度为M，时间复杂度请达到O(N+M)，额外空间复杂度请达到O(1)。
+ */
+public class MyCode_14_FindFirstIntersectNode {
+    public static void main(String[] args) {
+
+    }
+
+    public static class Node {
+        public int value;
+        public Node next;
+        public Node rand;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+}
