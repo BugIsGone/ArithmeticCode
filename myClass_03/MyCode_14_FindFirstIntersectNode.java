266d440f64934fefffb132702ce9b907251fa802
shapemind@aliyun.com <shapemind@163.com>
2021-11-15 20:03:04 +0800

    20211112-CPC-1

--- myClass_03/MyCode_14_FindFirstIntersectNode.java
+++ myClass_03/MyCode_14_FindFirstIntersectNode.java
@@ -1,6 +1,7 @@
 package myClass_03;
 
 
+import basic_class_03.Code_14_FindFirstIntersectNode;
 
 /**
  * @author shapemind
@@ -11,19 +12,169 @@ package myClass_03;
  * 在本题中，单链表可能有环，也可能无环。给定两个单链表的头节点head1和head2，这两个链表可能相交，也可能不相交。
  * 请实现一个函数，如果两个链表相交，请返回相交的第一个节点；如果不相交，返回null即可。
  * 要求：如果链表1的长度为N，链表2的长度为M，时间复杂度请达到O(N+M)，额外空间复杂度请达到O(1)。
+ *
+ * 思路：
+ * 1.使用快慢指针遍历两个链表找出循环入口点loop1和loop2；
+ * 2.若loop1和loop2都为null，则两个链表都为无环链表
+ *      2.1 此时遍历list1和list2，得到len1、len2、Node（list1最后一个）、Node（list2的最后一个）
+ *      2.2 若Node（list1最后一个） ≠ Node（list2的最后一个），则list1和list2不可能相交
+ *      2.3 若Node（list1最后一个） = Node（list2的最后一个）时，则len长的先走（len长-len2）步，然后从这个位置开始，开始移动
+ *      2.4 若Node（list1其中一个） = Node（list2的其中一个）时，则此时就是第一个相交的Node
+ * 3.若loop1或loop2任一为null，另外一个不为null（一个有环、一个无环）
+ *      3.1 则不可能相交
+ * 4.若loop1和loop2都不为null（两个都有环）
+        4.1 若loop1=loop2时，往上找第一个的交点Node
+        4.2 若loop1≠loop2时，从loop1开始走一直到环结束
+            4.2.1 若loop（list1环中的一点）= loop2，则list1和list2在环内有相交，返回loop1或loop2
+            4.2.2 若loop（list1环中所有点）≠ loop2，则list1和list2环内无相交，环外也无相交
  */
 public class MyCode_14_FindFirstIntersectNode {
     public static void main(String[] args) {
+        // 1->2->3->4->5->6->7->null
+        Node head1 = new Node(1);
+        head1.next = new Node(2);
+        head1.next.next = new Node(3);
+        head1.next.next.next = new Node(4);
+        head1.next.next.next.next = new Node(5);
+        head1.next.next.next.next.next = new Node(6);
+        head1.next.next.next.next.next.next = new Node(7);
 
+        // 0->9->8->6->7->null
+        Node head2 = new Node(0);
+        head2.next = new Node(9);
+        head2.next.next = new Node(8);
+        head2.next.next.next = head1.next.next.next.next.next; // 8->6
+        System.out.println(getIntersectNode(head1, head2).value);
+
+        // 1->2->3->4->5->6->7->4...
+        head1 = new Node(1);
+        head1.next = new Node(2);
+        head1.next.next = new Node(3);
+        head1.next.next.next = new Node(4);
+        head1.next.next.next.next = new Node(5);
+        head1.next.next.next.next.next = new Node(6);
+        head1.next.next.next.next.next.next = new Node(7);
+        head1.next.next.next.next.next.next = head1.next.next.next; // 7->4
+
+        // 0->9->8->2...
+        head2 = new Node(0);
+        head2.next = new Node(9);
+        head2.next.next = new Node(8);
+        head2.next.next.next = head1.next; // 8->2
+        System.out.println(getIntersectNode(head1, head2).value);
+
+        // 0->9->8->6->4->5->6..
+        head2 = new Node(0);
+        head2.next = new Node(9);
+        head2.next.next = new Node(8);
+        head2.next.next.next = head1.next.next.next.next.next; // 8->6
+        System.out.println(getIntersectNode(head1, head2).value);
     }
 
     public static class Node {
         public int value;
         public Node next;
-        public Node rand;
 
         public Node(int data) {
             this.value = data;
         }
+    }
+
+    public static Node getIntersectNode(Node head1, Node head2) {
+        if (head1 == null || head2 == null) return null;
+        Node loop1 = getLoopNode(head1);
+        Node loop2 = getLoopNode(head2);
+        if (loop1 == null && loop2 == null) {
+            return noLoop(head1, head2);
+        }
+        if (loop1 != null && loop2 != null) {
+            return bothLoop(head1, loop1, head2, loop2);
+        }
+        return null;
+    }
+
+    private static Node bothLoop(Node head1, Node loop1, Node head2, Node loop2) {
+        Node cur1 = null;
+        Node cur2 = null;
+        if (loop1 == loop2) {
+            cur1 = head1;
+            cur2 = head2;
+            int n = 0;
+            while (cur1 != loop1) {
+                n++;
+                cur1 = cur1.next;
+            }
+            while (cur2 != loop2) {
+                n --;
+                cur2 = cur2.next;
+            }
+            cur1 = n > 0 ? head1 : head2;
+            cur2 = cur1 == head1 ? head2 : head1;
+            n = Math.abs(n);
+            while (n != 0) {
+                n--;
+                cur1 = cur1.next;
+            }
+            while (cur1 != cur2) {
+                cur1 = cur1.next;
+                cur2 = cur2.next;
+            }
+            return cur1;
+        } else {
+            cur1 = loop1.next;
+            while (cur1 != loop1) {
+                if (cur1 != loop2) {
+                    return loop1;
+                }
+                cur1 = cur1.next;
+            }
+            return null;
+        }
+    }
+
+    private static Node noLoop(Node head1, Node head2) {
+        if (head1 == null || head2 == null) return null;
+        Node cur1 = head1;
+        Node cur2 = head2;
+        int n = 0;
+        while (cur1.next != null) {
+            n++;
+            cur1 = cur1.next;
+        }
+        while (cur2.next != null) {
+            n--;
+            cur2 = cur2.next;
+        }
+        cur1 = n > 0 ? head1 : head2;
+        cur2 = cur1 == head1 ? head2 : head1;
+        n = Math.abs(n);
+        while (n != 0) {
+            n--;
+            cur1 = cur1.next;
+        }
+        while (cur1 != cur2) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
+    }
+
+    private static Node getLoopNode(Node head) {
+        if (head == null || head.next == null || head.next.next == null) return null;
+        Node n1 = head.next;
+        Node n2 = head.next.next;
+        while (n1 != n2) {
+            if (n2.next == null || n2.next.next == null) {
+                return null;
+            }
+            n1 = n1.next;
+            n2 = n2.next.next;
+        }
+        n2 = head;
+        while (n1 != n2) {
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        return n1;
     }
 }
