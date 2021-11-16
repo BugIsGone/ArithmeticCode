8d0bc4d0699abfd0a90431b7a04e02e5cd96a71a
shapemind@aliyun.com <shapemind@163.com>
2021-11-16 18:56:37 +0800

    20211116-CPC-1

---
+++ basic_class_03/Code_16_Bloom.java
@@ -0,0 +1,14 @@
+package basic_class_03;
+
+/**
+ * @author shapemind
+ * @create 2021-11-16 12:46
+ *
+ * 布隆过滤器应用提示：
+ * 1.空间要求苛刻
+ * 2.样本量极大
+ * 3.黑名单问题
+ * 4.是否允许有错误率
+ */
+public class Code_16_Bloom {
+}
